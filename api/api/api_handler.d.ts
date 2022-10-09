e31ffbedee859936bd9a8fd8c7685bb7eea71e01
Peter Prog <peter.prog@yahoo.com>
2022-10-09 20:34:51 +0700

    ...

---
+++ api/api/api_handler.d.ts
@@ -0,0 +1,1 @@
+export function installHandler(app: any): void;
---
+++ api/api/respObjects/issue.d.ts
@@ -0,0 +1,19 @@
+export function list(_: any, { status, effortMin, effortMax }: {
+    status: any;
+    effortMin: any;
+    effortMax: any;
+}): Promise<any>;
+export function add(_: any, { issue }: {
+    issue: any;
+}): Promise<any>;
+export function get(_: any, { id }: {
+    id: any;
+}): Promise<any>;
+export function update(_: any, { id, changes }: {
+    id: any;
+    changes: any;
+}): Promise<any>;
+declare function remove(_: any, { id }: {
+    id: any;
+}): Promise<boolean | undefined>;
+export { remove as delete };
