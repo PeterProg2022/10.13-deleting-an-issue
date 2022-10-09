d9392364b58d879188aee95d9ac22dd897e5ea0e
Peter Prog <peter.prog@yahoo.com>
2022-10-09 09:39:19 +0700

    filestruktur für vercel

--- api/api/index.js
+++ api/api/index.js
@@ -11,7 +11,7 @@ const port = process.env.API_SERVER_PORT || 3000;
 
 (async function start() {
   try {
-    // await connectToDb();
+    await connectToDb();
     app.listen(port, () => {
       console.log(`API server started on port ${port}`);
     });
--- api/api/issue.js
+++ api/api/issue.js
@@ -1,42 +1,26 @@
 const { UserInputError } = require('apollo-server-express');
-// const { getDb, getNextSequence } = require('./db.js');
-
-const issuesDB = [
-  {
-    id: 1, status: 'New', owner: 'Ravan', effort: 5,
-    created: new Date('2019-01-15'), due: undefined,
-    title: 'Error in console when clicking Add',
-  },
-  {
-    id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
-    created: new Date('2019-01-16'), due: new Date('2019-02-01'),
-    title: 'Missing bottom border on panel',
-  },
-];
-
-
+const { getDb, getNextSequence } = require('./db.js');
 
 async function get(_, { id }) {
-  // const db = getDb();
-  // const issue = await db.collection('issues').findOne({ id });
-  // return issue;
-  return issuesDB[0]
+  const db = getDb();
+  const issue = await db.collection('issues').findOne({ id });
+  return issue;
 }
 
 async function list(_, { status, effortMin, effortMax }) {
-  // const db = getDb();
-  // const filter = {};
-  //
-  // if (status) filter.status = status;
-  //
-  // if (effortMin !== undefined || effortMax !== undefined) {
-  //   filter.effort = {};
-  //   if (effortMin !== undefined) filter.effort.$gte = effortMin;
-  //   if (effortMax !== undefined) filter.effort.$lte = effortMax;
-  // }
-  //
-  // const issues = await db.collection('issues').find(filter).toArray();
-  // return issues;
+  const db = getDb();
+  const filter = {};
+
+  if (status) filter.status = status;
+
+  if (effortMin !== undefined || effortMax !== undefined) {
+    filter.effort = {};
+    if (effortMin !== undefined) filter.effort.$gte = effortMin;
+    if (effortMax !== undefined) filter.effort.$lte = effortMax;
+  }
+
+  const issues = await db.collection('issues').find(filter).toArray();
+  return issues;
 }
 
 function validate(issue) {
@@ -53,45 +37,42 @@ function validate(issue) {
 }
 
 async function add(_, { issue }) {
-  // const db = getDb();
-  // validate(issue);
-  //
-  // const newIssue = Object.assign({}, issue);
-  // newIssue.created = new Date();
-  // newIssue.id = await getNextSequence('issues');
-  //
-  // const result = await db.collection('issues').insertOne(newIssue);
-  // const savedIssue = await db.collection('issues')
-  //   .findOne({ _id: result.insertedId });
-  // return savedIssue;
-    return issuesDB;
+  const db = getDb();
+  validate(issue);
+
+  const newIssue = Object.assign({}, issue);
+  newIssue.created = new Date();
+  newIssue.id = await getNextSequence('issues');
+
+  const result = await db.collection('issues').insertOne(newIssue);
+  const savedIssue = await db.collection('issues')
+    .findOne({ _id: result.insertedId });
+  return savedIssue;
 }
 
 async function update(_, { id, changes }) {
-  // const db = getDb();
-  // if (changes.title || changes.status || changes.owner) {
-  //   const issue = await db.collection('issues').findOne({ id });
-  //   Object.assign(issue, changes);
-  //   validate(issue);
-  // }
-  // await db.collection('issues').updateOne({ id }, { $set: changes });
-  // const savedIssue = await db.collection('issues').findOne({ id });
-  // return savedIssue;
-  return issuesDB
+  const db = getDb();
+  if (changes.title || changes.status || changes.owner) {
+    const issue = await db.collection('issues').findOne({ id });
+    Object.assign(issue, changes);
+    validate(issue);
+  }
+  await db.collection('issues').updateOne({ id }, { $set: changes });
+  const savedIssue = await db.collection('issues').findOne({ id });
+  return savedIssue;
 }
 
 async function remove(_, { id }) {
-  // const db = getDb();
-  // const issue = await db.collection('issues').findOne({ id });
-  // if (!issue) return false;
-  // issue.deleted = new Date();
-  //
-  // let result = await db.collection('deleted_issues').insertOne(issue);
-  // if (result.insertedId) {
-  //   result = await db.collection('issues').removeOne({ id });
-  //   return result.deletedCount === 1;
-  // }
-  return false;
+  const db = getDb();
+  const issue = await db.collection('issues').findOne({ id });
+  if (!issue) return false;
+  issue.deleted = new Date();
+
+  let result = await db.collection('deleted_issues').insertOne(issue);
+  if (result.insertedId) {
+    result = await db.collection('issues').removeOne({ id });
+    return result.deletedCount === 1;
+  }
 }
 
 module.exports = {
